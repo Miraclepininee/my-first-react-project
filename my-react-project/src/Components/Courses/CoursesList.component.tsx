d88e8b5cd6558a50ad178706622684966d2b5a68
Miracle pininee siddert Saturday <110922869+miraclesiddert@users.noreply.github.com>
2023-09-26 07:28:19 -0700

    adding my first react app
    Learning how to create a repo

---
+++ my-react-project/src/Components/Courses/CoursesList.component.tsx
@@ -0,0 +1,23 @@
+// example using class extending component
+import React from "react";
+
+export class CoursesListComponent extends React.Component<{
+  items: any[];
+}> {
+  constructor(props: { items: any[] }) {
+    super(props);
+  }
+  render(): React.ReactNode {
+    const { items } = this.props;
+    return (
+      <div>
+        <h3>Items:</h3>
+        <ul>
+          {items.map((item: any, index: number) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
